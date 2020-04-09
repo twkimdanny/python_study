3f45a99c5eca339510c528bcc13a18ff4dc5f1b8
twkim <twkim.danny@gmail.com>
2020-04-09 15:40:00 +0900

    init
    init commit

---
+++ gugudan.py
@@ -0,0 +1,9 @@
+def gugudan(dan) :
+    # print('구구단 몇단? ' + str(dan) + '단입니다.')
+    print('구구단 몇단? ', dan, '단입니다.')
+    i = 1
+    while(i < 10) :
+        print(dan, 'x', i, '=', dan * i)
+        i = i + 1
+    
+gugudan(3)
